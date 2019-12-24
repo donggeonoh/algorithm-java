00b0fa6287720d65bb0db4df50c83ca57c167473
Oh Dong-geon <donggeon.dev@gmail.com>
2019-12-24 21:40:40 +0900

    Create PhoneBook.java

---
+++ Programmers/src/dan/programmers/solution/hash/PhoneBook.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+
+class PhoneBook {
+    public boolean solution(String[] phone_book) {
+        HashMap<String,Integer> p = new HashMap();
+        boolean answer = true;
+        
+        for(int i = 0; i < phone_book.length; i++) {
+            for(int j = 0; j < phone_book.length; j++) {
+                
+                if(i == j) continue;
+                
+                if(phone_book[j].indexOf(phone_book[i]) == 0) {
+                    return false;   
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
