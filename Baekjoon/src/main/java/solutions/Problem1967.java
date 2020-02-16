5c3e7d97972a5b378041a28cbeb37782ab70d096
Oh Dong-geon <donggeon.dev@gmail.com>
2020-02-16 23:30:44 +0900

    트리 지름 구하기 구현중

--- Baekjoon/src/main/java/solutions/Problem1967.java
+++ Baekjoon/src/main/java/solutions/Problem1967.java
@@ -1,15 +1,99 @@
 package solutions;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Problem1967 {
 
-    RootNode rootNode;
+    List<List<MyNode>> nodes;
 
-    public int solution(int[][] input) {
-        rootNode = new RootNode(input);
-        return 45;
+    MyNode farthestNode;
+    int farthestLength = 0;
+
+    int num;
+    int answer;
+
+    public int solution(int num, int[][] input) {
+
+        this.nodes = new LinkedList<>();
+        this.num = num;
+
+        init(input);
+
+        MyNode node = getNodeWithFarthestDistance(nodes.get(0).get(0), 0);
+        MyNode nextNode = getNodeWithFarthestDistance(node, 0);
+
+        return answer;
+    }
+
+    private MyNode getNodeWithFarthestDistance(MyNode node, int length) {
+
+        if(nextNode() == null) {
+            return null;
+        }
+
+        if(length > farthestLength) {
+            farthestNode = node;
+            farthestLength = length;
+            return farthestNode;
+        }
+
+        length += node.getEdge();
+
+        return null;
+    }
+
+    private MyNode nextNode() {
+
+    }
+
+
+    private void init(int[][] input) {
+
+        for (int i = 0; i < num; i++) {
+            if (input[i][0] > nodes.size()) {
+                nodes.add(new LinkedList<>());
+            }
+            nodes.get(input[i][0] - 1).add(new MyNode(input[i][0], input[i][1], input[i][2]));
+        }
     }
 }
 
+class MyNode {
+
+    private int depth;
+    private int vertex;
+    private int edge;
+
+    public MyNode(int depth, int vertex, int edge) {
+        this.depth = depth;
+        this.vertex = vertex;
+        this.edge = edge;
+    }
+
+    public int getDepth() {
+        return depth;
+    }
+
+    public int getVertex() {
+        return vertex;
+    }
+
+    public int getEdge() {
+        return edge;
+    }
+
+    @Override
+    public String toString() {
+        return "MyNode{" +
+                "depth=" + depth +
+                ", vertex=" + vertex +
+                ", edge=" + edge +
+                '}';
+    }
+}
+
+/*
 class RootNode {
 
     int size;
@@ -30,6 +114,8 @@ class RootNode {
         for (int i = 0; i < input.length; i++) {
             if(count == input[i][0]) {
                 node = newNode(input[i][0], input[i][1], input[i][2]);
+
+                count++;
             }
             else {
                 count++;
@@ -87,3 +173,4 @@ class Node {
         return right;
     }
 }
+*/
--- Baekjoon/src/test/java/solutions/Problem1967Test.java
+++ Baekjoon/src/test/java/solutions/Problem1967Test.java
@@ -2,17 +2,33 @@ package solutions;
 
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 import static org.junit.Assert.assertEquals;
 
 public class Problem1967Test {
 
     @Test
-    public void test() {
+    public void test() throws FileNotFoundException {
+
         Problem1967 problem1967 = new Problem1967();
-        InputTestcase input = new InputTestcase("/Users/donggeon/Sources/Algorithm/Baekjoon/src/main/java/testcases/Problem1967Input.txt");
-        int answer = problem1967.solution(input.inputTwoDimensionalArray(3));
+        Scanner scanner = new Scanner(new File("/Users/donggeon/Sources/Algorithm/Baekjoon/src/main/java/testcases/Problem1967Input.txt"));
+
+        int num = scanner.nextInt() - 1;
+        int[][] input = new int[num][3];
+
+        for(int i = 0; i < input.length; i++) {
+            for(int j = 0; j < input[i].length; j++) {
+                input[i][j] = scanner.nextInt();
+                System.out.print(input[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        int answer = problem1967.solution(num, input);
         assertEquals(45, answer);
-        input.close();
     }
 
 }
