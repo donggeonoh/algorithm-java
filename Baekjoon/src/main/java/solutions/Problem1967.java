c45b2c04ff85be06e696b6f9995f909b5f926201
Oh Dong-geon <donggeon.dev@gmail.com>
2020-02-18 23:55:27 +0900

    백준 1967번 알고리즘 입력 초기화

--- Baekjoon/src/main/java/solutions/Problem1967.java
+++ Baekjoon/src/main/java/solutions/Problem1967.java
@@ -5,74 +5,59 @@ import java.util.List;
 
 public class Problem1967 {
 
-    List<List<MyNode>> nodes;
+    private List<List<MyNode>> nodes;
+    private int size;
+    private boolean[] visited;
 
-    MyNode farthestNode;
-    int farthestLength = 0;
-
-    int num;
-    int answer;
-
-    public int solution(int num, int[][] input) {
+    public Problem1967(int size, int[][] input) {
 
         this.nodes = new LinkedList<>();
-        this.num = num;
+        this.size = size;
+        this.visited = new boolean[size];
 
         init(input);
-
-        MyNode node = getNodeWithFarthestDistance(nodes.get(0).get(0), 0);
-        MyNode nextNode = getNodeWithFarthestDistance(node, 0);
-
-        return answer;
     }
 
-    private MyNode getNodeWithFarthestDistance(MyNode node, int length) {
+    public int printSolution() {
 
-        if(nextNode() == null) {
-            return null;
+        getFarthestNode(0);
+
+        return 0;
+    }
+
+    private void getFarthestNode(int vIndex) {
+
+        if(visited[vIndex]) {
+            return;
         }
-
-        if(length > farthestLength) {
-            farthestNode = node;
-            farthestLength = length;
-            return farthestNode;
-        }
-
-        length += node.getEdge();
-
-        return null;
     }
-
-    private MyNode nextNode() {
-        return null;
-    }
-
 
     private void init(int[][] input) {
 
-        for (int i = 0; i < num; i++) {
-            if (input[i][0] > nodes.size()) {
+        for(int[] node : input) {
+            if(node[0] >= nodes.size()) {
                 nodes.add(new LinkedList<>());
             }
-            nodes.get(input[i][0] - 1).add(new MyNode(input[i][0], input[i][1], input[i][2]));
+            nodes.get(node[0] - 1).add(new MyNode(node[1], node[2]));
+        }
+
+        for(int[] node : input) {
+            if(node[1] >= nodes.size()) {
+                nodes.add(new LinkedList<>());
+            }
+            nodes.get(node[1] - 1).add(new MyNode(node[0], node[2]));
         }
     }
 }
 
 class MyNode {
 
-    private int depth;
     private int vertex;
     private int edge;
 
-    public MyNode(int depth, int vertex, int edge) {
-        this.depth = depth;
+    public MyNode(int vertex, int edge) {
         this.vertex = vertex;
         this.edge = edge;
-    }
-
-    public int getDepth() {
-        return depth;
     }
 
     public int getVertex() {
@@ -82,95 +67,4 @@ class MyNode {
     public int getEdge() {
         return edge;
     }
-
-    @Override
-    public String toString() {
-        return "MyNode{" +
-                "depth=" + depth +
-                ", vertex=" + vertex +
-                ", edge=" + edge +
-                '}';
-    }
 }
-
-/*
-class RootNode {
-
-    int size;
-
-    private Node left;
-    private Node right;
-
-    RootNode(int[][] input) {
-        size = input.length;
-        init(input);
-    }
-
-    private void init(int[][] input) {
-
-        Node node;
-        int count = 1;
-
-        for (int i = 0; i < input.length; i++) {
-            if(count == input[i][0]) {
-                node = newNode(input[i][0], input[i][1], input[i][2]);
-
-                count++;
-            }
-            else {
-                count++;
-
-            }
-        }
-    }
-
-    private Node newNode(int depth, int vertex, int edge) {
-
-        if(left == null) {
-            left = new Node(depth, vertex, edge);
-            return left;
-        } else {
-            right = new Node(depth, vertex, edge);
-            return right;
-        }
-    }
-
-}
-
-class Node {
-
-    private int depth;
-
-    private int vertex;
-    private int edge;
-
-    private Node left;
-    private Node right;
-
-    public Node(int depth, int vertex, int edge) {
-        this.depth = depth;
-        this.vertex = vertex;
-        this.edge = edge;
-    }
-
-    public int getDepth() {
-        return depth;
-    }
-
-    public int getVertex() {
-        return vertex;
-    }
-
-    public int getEdge() {
-        return edge;
-    }
-
-    public Node getLeft() {
-        return left;
-    }
-
-    public Node getRight() {
-        return right;
-    }
-}
-*/
--- Baekjoon/src/test/java/solutions/Problem1967Test.java
+++ Baekjoon/src/test/java/solutions/Problem1967Test.java
@@ -13,7 +13,6 @@ public class Problem1967Test {
     @Test
     public void test() throws FileNotFoundException {
 
-        Problem1967 problem1967 = new Problem1967();
         Scanner scanner = new Scanner(new File("/Users/donggeon/Sources/Algorithm/Baekjoon/src/main/java/testcases/Problem1967Input.txt"));
 
         int num = scanner.nextInt() - 1;
@@ -22,13 +21,10 @@ public class Problem1967Test {
         for(int i = 0; i < input.length; i++) {
             for(int j = 0; j < input[i].length; j++) {
                 input[i][j] = scanner.nextInt();
-                System.out.print(input[i][j] + " ");
             }
-            System.out.println();
         }
 
-        int answer = problem1967.solution(num, input);
-        assertEquals(45, answer);
+        Problem1967 problem1967 = new Problem1967(num, input);
     }
 
 }
