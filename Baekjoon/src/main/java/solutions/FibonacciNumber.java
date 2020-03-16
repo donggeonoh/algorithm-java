8cb5cab6b0cae0a97c38c3092f4246c3ce5f14b6
Oh-Donggeon <donggeon.dev@gmail.com>
2020-03-16 22:21:59 +0900

    백준 피보나치의 수 알고리즘 구현

--- Baekjoon/src/main/java/solutions/FibonacciNumber.java
+++ Baekjoon/src/main/java/solutions/FibonacciNumber.java
@@ -1,11 +1,37 @@
 package solutions;
 
+import java.util.InputMismatchException;
+
 /**
  * Created by ohdonggeon on 2020/03/16 9:32 PM
  * Dankook UNIV. Computer Science
+ * Source : https://www.acmicpc.net/problem/10870
+ * Desc : 파보나치의 수를 구하시오. (재귀 구현)
+ * Input : N (N >= 2)
+ * Output : N 번째 파보나치의 수
  */
 public class FibonacciNumber {
 
+	private final int input;
 
+	FibonacciNumber(int input) {
+		if (input < 0) {
+			throw new InputMismatchException("The input value must be greater than zero.");
+		}
+		this.input = input;
+	}
 
+	public void printSolution() {
+		System.out.println(getAnswer(input));
+	}
+
+	private int getAnswer(int input) {
+		if (input == 1) {
+			return 1;
+		} else if (input == 0) {
+			return 0;
+		}
+
+		return getAnswer(input - 1) + getAnswer(input - 2);
+	}
 }
