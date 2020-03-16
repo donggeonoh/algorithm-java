ae12e774ee3b222c450c8220cc7b519721606751
Oh-Donggeon <donggeon.dev@gmail.com>
2020-03-16 21:33:46 +0900

    팩토리얼 주석 수정

--- Baekjoon/src/main/java/solutions/Factorial.java
+++ Baekjoon/src/main/java/solutions/Factorial.java
@@ -7,7 +7,7 @@ import java.util.InputMismatchException;
  * Created by ohdonggeon on 2020/03/16 8:11 PM
  * Dankook UNIV. Computer Science
  * Source : https://www.acmicpc.net/problem/10872
- * Desc : 팩토리얼 값을 구하시오.
+ * Desc : 팩토리얼 값을 구하시오. (재귀로 구현)
  * Input : 정수 N(0 <= N <= 12)
  * Output : N!
  */
---
+++ Baekjoon/src/main/java/solutions/FibonacciNumber.java
@@ -0,0 +1,11 @@
+package solutions;
+
+/**
+ * Created by ohdonggeon on 2020/03/16 9:32 PM
+ * Dankook UNIV. Computer Science
+ */
+public class FibonacciNumber {
+
+
+
+}
