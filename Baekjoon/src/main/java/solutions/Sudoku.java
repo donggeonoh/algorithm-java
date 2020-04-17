d3f12ed5c7518bfe2d71c9a152ebd19b162b571c
Oh-Donggeon <donggeon.dev@gmail.com>
2020-04-17 22:23:40 +0900

    백준 알고리즘 스도쿠 구현 중

--- Baekjoon/src/main/java/solutions/Sudoku.java
+++ Baekjoon/src/main/java/solutions/Sudoku.java
@@ -1,5 +1,11 @@
 package solutions;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 /**
  * Created by ohdonggeon on 2020/04/06 10:38 PM
  * Dankook UNIV. Computer Science
@@ -10,6 +16,9 @@ package solutions;
  */
 public class Sudoku {
 
+	public static final int SIZE = 9;
+	public static final int EMPTY = 0;
+
 	private static int[][] sudoku;
 
 	private Sudoku() {
@@ -19,10 +28,99 @@ public class Sudoku {
 	public static void printSolution(int[][] input) {
 		sudoku = input;
 
-		printSolution();
+		printSolution(0, 0);
 	}
 
-	private static void printSolution() {
+	private static void printSolution(int row, int col) {
+		if(row == SIZE) {
+			System.out.println(Arrays.deepToString(sudoku));
+			return;
+		}
 
+		if(col == SIZE) {
+			printSolution(row + 1, 0);
+			return;
+		}
+
+		for (int sRow = row; sRow < SIZE; sRow++) {
+			for (int sCol = col; sCol < SIZE; sCol++) {
+				if (sudoku[row][col] == EMPTY) {
+					for (int num = 1; num <= 9; num++) {
+						if (isPossible(row, col, num)) {
+							sudoku[row][col] = num;
+							printSolution(row, col + 1);
+						}
+					}
+				}
+			}
+		}
+	}
+
+	private static boolean isPossible(int row, int col, int sNum) {
+		if (isSameNumberHorizontal(row, sNum)) {
+			return false;
+		}
+
+		if (isSameNumberVertical(col, sNum)) {
+			return false;
+		}
+
+		if (isSameNumberRange(row, col, sNum)) {
+			return false;
+		}
+
+		return true;
+	}
+
+	private static boolean isSameNumberHorizontal(int row, int num) {
+		for (int col = 0; col < SIZE; col++) {
+			if (sudoku[row][col] == num) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	private static boolean isSameNumberVertical(int col, int num) {
+		for (int row = 0; row < SIZE; row++) {
+			if (sudoku[row][col] == num) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	private static boolean isSameNumberRange(int row, int col, int num) {
+		row = (row / 3) * 3;
+		col = (col / 3) * 3;
+
+		for (int sRow = row; sRow < row + 3; sRow++) {
+			for (int sCol = col; sCol < col + 3; sCol++) {
+				if (sudoku[sRow][sCol] == num) {
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+}
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		int[][] input = new int[Sudoku.SIZE][Sudoku.SIZE];
+
+		for (int row = 0; row < Sudoku.SIZE; row++) {
+			StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+			for (int col = 0; col < Sudoku.SIZE; col++) {
+				input[row][col] = Integer.parseInt(tokenizer.nextToken());
+			}
+		}
+
+		Sudoku.printSolution(input);
+		reader.close();
 	}
 }
--- Baekjoon/src/test/java/solutions/SudokuTest.java
+++ Baekjoon/src/test/java/solutions/SudokuTest.java
@@ -2,7 +2,8 @@ package solutions;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.io.*;
+import java.util.StringTokenizer;
 
 /**
  * Created by ohdonggeon on 2020/04/06 10:41 PM
@@ -11,7 +12,20 @@ import static org.junit.Assert.*;
 public class SudokuTest {
 
 	@Test
-	public void printSolution() {
+	public void printSolution() throws IOException {
+		BufferedReader reader = new BufferedReader(new FileReader(new File("/Users/ohdonggeon/Sources/Algorithm/Baekjoon/src/test/java/testcases/SudokuInput.txt")));
+		int[][] input = new int[Sudoku.SIZE][Sudoku.SIZE];
 
+		for (int row = 0; row < Sudoku.SIZE; row++) {
+			StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+			for (int col = 0; col < Sudoku.SIZE; col++) {
+				input[row][col] = Integer.parseInt(tokenizer.nextToken());
+			}
+		}
+
+
+
+		Sudoku.printSolution(input);
+		reader.close();
 	}
 }
